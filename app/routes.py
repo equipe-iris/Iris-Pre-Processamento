e0a1282e46ca455af894755dc7b4f595fdcb40f3
Matheus Melo <matheusfernando.vm@gmail.com>
2025-04-11 21:45:26 -0300

    feat: Alterando a rota para receber o arquivo.csv como upload

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, request, jsonify
 import pandas as pd
-from app.dataExtraction import dataExtraction
 
 bp = Blueprint('routes', __name__)
 
 @bp.route('/get-dataset', methods=['POST'])
-async def get_dataset():
+def get_dataset():
     try:
-        text = await dataExtraction.dataExtraction()
-        return jsonify({
-            "message": "Arquivo processado com sucesso."
-        }), 200
+        if 'file' not in request.files:
+            return jsonify({'erro': 'Nenhum arquivo enviado.'}), 400
+
+        file = request.files['file']
+
+        if file.filename != 'Jira Exportar CSV (todos os campos) 20250312084318.csv' or file.name != 'Chamados Porto.csv':
+            return jsonify({'erro': 'Nome do arquivo inválido.'}), 400
+        return jsonify({'mensagem': 'Arquivo recebido com sucesso.'}), 200
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({'error': str(e)}), 500
